2a1d3bb9b6ec56bb7c14ab128d9ac81161237e72
agorobtsov <AVGorobtsov@alfabank.ru>
2019-02-25 18:16:57 +0300

    webpack + babel + react

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './src'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output management',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    }
+};
