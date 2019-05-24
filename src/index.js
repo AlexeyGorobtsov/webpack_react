34f9d03e723fcec50a60966ae9c6f53e9c915018
Alexey <lexa2987@mail.ru>
2019-05-24 13:08:15 +0300

    add css loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {render } from 'react-dom';
 import { Provider } from 'react-redux';
+
 import { createStore } from 'redux';
 import todoApp from './redusers';
 import App from './components/App';
+import './styles.css';
 
 const store = createStore(todoApp);
 
