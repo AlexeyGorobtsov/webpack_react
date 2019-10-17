632e54e90284c7cd296a533e6a75519c8272e5aa
Alexey <lexa2987@mail.ru>
2019-10-17 12:06:11 +0300

    add production mode

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = new express();
 const config = require('./webpack.config');
 const compiler = webpack(config);
 
-const routes = require('./src/routes');
+
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
-}), routes);
+}));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,23 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: {
-        app: './src/index.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './src'
+        app: './src/index.js',
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Output management',
+            title: 'Production',
             template: './index.html'
         })
     ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './'
+    },
     module: {
         rules: [
             {
@@ -33,9 +33,4 @@ module.exports = {
             }
         ]
     },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    }
 };
